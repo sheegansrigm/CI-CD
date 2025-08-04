d79138c0ea4482e387a2a8a44dab69d771c8caef
Sheegan Sri G M <sheegangm@gmail.com>
2025-08-04 14:51:30 +0530

    Update

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello():
 
 @app.route("/health")
 def health():
-    return jsonify({"status": "ok"})
+    return jsonify({"status": "okok"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
