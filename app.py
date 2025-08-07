6a6a631a03192a758d573e8ae02282822949c0cd
Sheegan Sri G M <sheegangm@gmail.com>
2025-08-07 09:20:22 +0530

    dem 1

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello():
 
 @app.route("/health")
 def health():
-    return jsonify({"status": "okokokok"})
+    return jsonify({"status": "test"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
