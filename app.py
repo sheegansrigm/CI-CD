e558b7b01779a263a56b32ef1ba3f256d1f7a3f1
Sheegan Sri G M <sheegangm@gmail.com>
2025-08-22 11:56:58 +0530

    testing rollback

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello():
 
 @app.route("/health")
 def health():
-    return jsonify({"status": "test"})
+    return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
